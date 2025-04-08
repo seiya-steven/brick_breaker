4021f1dc5740faaf40948ed5ce95578d2cd002b7
hachiko <hoangthang121006@gmail.com>
2025-04-08 20:54:17 +0700

    update

--- defs.h
+++ defs.h
@@ -4,9 +4,9 @@
 const int SCREEN_WIDTH = 736;
 const int SCREEN_HEIGHT = 736;
 #define WINDOW_TITLE "Brick Breaker"
-#define SPEED 2
+#define SPEED 3
 #define BALL_SPEED 2
-#define BALL_SIZE 24
+#define BALL_SIZE 32
 #define COL 15
 #define ROW 3
 #define SPACING 24
